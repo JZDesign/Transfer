e490118167d1e79d345af059bab5b71f4e184e25
Jacob Rakidzich <jacob.rakidzich@daveramsey.com>
2020-12-11 21:55:44 -0600

    move readme and update docs

--- README.md
+++
@@ -1,5 +0,0 @@
-[![codebeat badge](https://codebeat.co/badges/d7b361b5-e7c9-45b7-8d5c-6eaffb058798)](https://codebeat.co/projects/github-com-jzdesign-transfer-main) [![SPM compatible](https://img.shields.io/badge/SPM-Compatible-e66f20.svg?style=flat)](https://github.com/apple/swift-package-manager) [![License](https://img.shields.io/badge/License-MIT-335577.svg?style=flat)](https://github.com/JZDesign/Transfer/blob/master/LICENSE)
-
-# Transfer
-
-Server Side HTML generation & templating in Swift
--- Tests/TransferTests/PageTests.swift
+++ Tests/TransferTests/PageTests.swift
@@ -10,5 +10,32 @@ final class PageTests: XCTestCase {
         let result = "<html lang=\"en\"><head><title>Title</title><link rel=\"stylesheet\" href=\"styles/someThing.css\"></link><link rel=\"icon\" href=\"favicon.ico\"></link></head><body><h1>This is your title</h1></body></html>"
         
         XCTAssertEqual(newPage.rendered(withBody: TransferElement(element: .h1, attributes: [], content: "This is your title")), result)
+        
     }
 }
+
+
+struct Body {
+    
+    func doX() {
+        let someDivContent = TransferElement(element: "", content: "")
+        
+        let body = [
+            TransferElement(element: .h1, content: "This is your title"),
+            TransferElement(element: .div, attributes: [.class(value: "container")], content: someDivContent)
+        ]
+        
+        Page(
+            head: Head(
+                title: "Title", headContent:
+                        .styleSheet("styles/someThing.css"),
+                       .favicon()
+            )
+        ).rendered(withBody: body)
+        
+        
+    }
+    
+}
+
+
